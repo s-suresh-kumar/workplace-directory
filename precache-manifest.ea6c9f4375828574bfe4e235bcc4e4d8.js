ec95e782d12234ac0ccae81d759d9e203e5405e7
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-09-26 17:22:41 -0400

    Updates

---
+++ precache-manifest.ea6c9f4375828574bfe4e235bcc4e4d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf39469e6149e49ec5bafc5908fc723",
+    "url": "/workplace-directory/index.html"
+  },
+  {
+    "revision": "fa46be9a0958f547f27a",
+    "url": "/workplace-directory/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "6100a14fba882803b06e",
+    "url": "/workplace-directory/static/css/main.d8494276.chunk.css"
+  },
+  {
+    "revision": "fa46be9a0958f547f27a",
+    "url": "/workplace-directory/static/js/2.808f6ea4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/workplace-directory/static/js/2.808f6ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6100a14fba882803b06e",
+    "url": "/workplace-directory/static/js/main.ee04570f.chunk.js"
+  },
+  {
+    "revision": "4e10759726594db17cfe",
+    "url": "/workplace-directory/static/js/runtime-main.8695945b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workplace-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
